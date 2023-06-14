01801ce18f298a985a5d13a593d35f257a8172d8
Chris Humphry <chhu@pop-os.localdomain>
2023-06-14 09:51:03 -0400

    Added manifest file for registration module

---
+++ kauiil_motors_motorcycle_registration/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': """Motorcycle Registry
+        ====================
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.""",
+    'author': 'chhu-odoo',
+    'website': 'https://github.com/chhu-odoo/custom_addons',
+    'category': 'Kawiil/Motorcycle Registry',
+    'depends': ['base'],
+    'license': 'OEEL-1',
+    'data': [],
+    'demo': [],
+    'application': True
+}
